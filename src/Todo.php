c7f1692cbf317370067e84a1872700b863abf7fd
Memory Clutter <memclutter@gmail.com>
2015-12-10 00:27:11 +0300

    Annotate Todo properties

--- src/Todo.php
+++ src/Todo.php
@@ -2,6 +2,16 @@
 
 namespace memclutter\PhpTodo;
 
+/**
+ * Class Todo
+ *
+ * @property int $id
+ * @property string $text
+ * @property int $status
+ * @property int $priority
+ * @property string $created
+ * @property string $updated
+ */
 class Todo
 {
     use ActiveRecordTrait;
