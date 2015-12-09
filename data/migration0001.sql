c6c6a0cd9c5bdf485a42c91920fb0cf22e04e3df
Memory Clutter <memclutter@gmail.com>
2015-12-09 22:38:36 +0300

    First migration

---
+++ data/migration0001.sql
@@ -0,0 +1,6 @@
+INSERT INTO `todo` (`id`, `text`, `status`, `priority`, `created`, `updated`) VALUES
+  (1, 'Send a letter', 0, 2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (2, 'Go to the grocery store', 0, 4, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (3, 'Go to an English language course', 0, 4, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (4, 'Read To Meet with a friend', 0, 2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+  (5, 'Repair washing machine', 0, 4, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
