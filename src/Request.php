dcf4e3dd73d81f644236739a42e256e1acc27740
Memory Clutter <memclutter@gmail.com>
2015-12-12 13:12:36 +0300

    Move getClientIp() in Utils class

--- src/Request.php
+++ src/Request.php
@@ -41,12 +41,6 @@ class Request
 
     public function getClientIp()
     {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            return $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            return $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            return $_SERVER['REMOTE_ADDR'];
-        }
+        return Utils::getClientIp();
     }
 }
--- src/Utils.php
+++ src/Utils.php
@@ -42,4 +42,15 @@ class Utils
 
         return $arr;
     }
+
+    public static function getClientIp()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+    }
 }
