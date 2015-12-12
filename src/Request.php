553c9968f5b446f552aac19364c224ca17b1abea
Memory Clutter <memclutter@gmail.com>
2015-12-12 13:10:38 +0300

    Adding Request::getClientIp() method

--- src/Request.php
+++ src/Request.php
@@ -38,4 +38,15 @@ class Request
 
         return trim($path, '/');
     }
+
+    public function getClientIp()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+    }
 }
