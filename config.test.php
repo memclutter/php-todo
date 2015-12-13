0ab08a0f82de9d1da11e692d9905b9d6450ecd38
Memory Clutter <memclutter@gmail.com>
2015-12-13 12:38:06 +0300

    Support travis ci

---
+++ config.test.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'pdo' => [
+        'dsn' => 'mysql:dbname=travis_ci_memclutter_php_todo;charset=utf8',
+        'username' => 'memclutter_php_todo',
+        'passwd' => '',
+    ],
+];
