739c0865d1582f20732a0c66945ed39e2d92f1ad
Memory Clutter <memclutter@gmail.com>
2015-12-13 12:40:46 +0300

    Update travis ci mysql user

--- config.test.php
+++ config.test.php
@@ -3,7 +3,7 @@
 return [
     'pdo' => [
         'dsn' => 'mysql:dbname=travis_ci_memclutter_php_todo;charset=utf8',
-        'username' => 'memclutter_php_todo',
+        'username' => 'root',
         'passwd' => '',
     ],
 ];
